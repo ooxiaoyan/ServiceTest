d75e9fce472033f99aabac69602f260a63327007
ooxiaoyan <810978527@qq.com>
2018-09-18 09:33:34 +0800

    Add two buttons to test start and stop service

--- app/src/main/java/com/example/servicetest/MainActivity.java
+++ app/src/main/java/com/example/servicetest/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.servicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startService = findViewById(R.id.start_service);
+        Button stopService = findViewById(R.id.stop_service);
+
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);//启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);//停止服务
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/servicetest/MyService.java
+++ app/src/main/java/com/example/servicetest/MyService.java
@@ -3,8 +3,12 @@ package com.example.servicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
+
+    private static final String TAG = "MyService";
+
     public MyService() {
     }
 
@@ -17,15 +21,18 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate: executed");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: executed");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy: executed");
     }
 }
